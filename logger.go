1bae08ce776e64650be074680a7b468d7a29d792
Jourloy <jourloy@yandex.ru>
2022-01-04 19:33:18 +0300

    Update

--- logger.go
+++ logger.go
@@ -12,13 +12,13 @@ var l int
 
 func Log(t string) {
 	timeNow := time.Now()
-	log := timeNow.Format(time.UnixDate) + "   " + n + "   [LOG] - " + t
+	log := timeNow.Format(time.UnixDate) + "  " + n + "   [LOG] - " + t
 	fmt.Println(cl.ChangeFG(log, "green"))
 }
 
 func Warn(t string) {
 	timeNow := time.Now()
-	log := timeNow.Format(time.UnixDate) + "   " + n + "  [WARN] - " + t
+	log := timeNow.Format(time.UnixDate) + "  " + n + "  [WARN] - " + t
 	fmt.Println(cl.ChangeFG(log, "yellow"))
 }
 
@@ -27,13 +27,13 @@ func Info(t string) {
 		return
 	}
 	timeNow := time.Now()
-	log := timeNow.Format(time.UnixDate) + "   " + n + "  [INFO] - " + t
+	log := timeNow.Format(time.UnixDate) + "  " + n + "  [INFO] - " + t
 	fmt.Println(cl.ChangeFG(log, "grey"))
 }
 
 func Error(t string) {
 	timeNow := time.Now()
-	log := timeNow.Format(time.UnixDate) + "   " + n + " [Error] - " + t
+	log := timeNow.Format(time.UnixDate) + "  " + n + " [ERROR] - " + t
 	fmt.Println(cl.ChangeFG(log, "red"))
 }
 
@@ -42,7 +42,7 @@ func Debug(t string) {
 		return
 	}
 	timeNow := time.Now()
-	log := timeNow.Format(time.UnixDate) + "   " + n + " [Debug] - " + t
+	log := timeNow.Format(time.UnixDate) + "  " + n + " [DEBUG] - " + t
 	fmt.Println(cl.ChangeFG(log, "magenta"))
 }
 
