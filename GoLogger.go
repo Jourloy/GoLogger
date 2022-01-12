2c30c57e355862e5b7cf9c1777e904fa175fc95a
Igor Shaposhnikov <shaposhnikov.i@leadrock.com>
2022-01-12 04:06:38 +0300

    Recode logger

---
+++ GoLogger.go
@@ -0,0 +1,137 @@
+package GoLogger
+
+import (
+	"fmt"
+	"time"
+
+	cl "github.com/Jourloy/Colored"
+)
+
+type Logger struct {
+	l int
+	n string
+}
+
+// Create logger
+//
+// Levels
+//
+// 1 - Error
+//
+// 2 - Warn
+//
+// 3 - Log
+//
+// 4 - Info
+//
+// 5 - Debug
+//
+// Example
+//
+//     // For all levels
+//     logger := GoLogger.New(5, "Logger")
+//     // For only important levels
+//     logger := GoLogger.New(3, "Logger")
+func New(level int, name string) Logger {
+	logger := Logger{}
+	logger.l = level
+	logger.n = name
+	return logger
+}
+
+// You can increase or degrease level for your logger
+//
+// Example
+//
+//     // Increase level on 2
+//     logger = logger.Level(2)
+//     // Degrease level on 1
+//     logger = logger.Level(-1)
+func (l Logger) Level(level int) Logger {
+	l.l += level
+	if l.l < 1 {
+		l.l = 1
+	} else if l.l > 5 {
+		l.l = 5
+	}
+	return l
+}
+
+// Important red messages when something go wrong
+//
+// Level: 1
+//
+// Example
+//
+//     logger.Error("User not found")
+func (l Logger) Error(t string) bool {
+	if l.l < 1 {
+		return false
+	}
+	timeNow := time.Now()
+	log := timeNow.Format(time.UnixDate) + "  " + l.n + " [ERROR] - " + t
+	fmt.Println(cl.ChangeFG(log, "red"))
+	return true
+}
+
+// Orange messages when something need your attention
+//
+// Level: 2
+//
+// Example
+//
+//     logger.Warn("Duplicated")
+func (l Logger) Warn(t string) bool {
+	if l.l < 2 {
+		return false
+	}
+	timeNow := time.Now()
+	log := timeNow.Format(time.UnixDate) + "  " + l.n + "  [WARN] - " + t
+	fmt.Println(cl.ChangeFG(log, "yellow"))
+	return true
+}
+
+// Green messages for success or just useful moments
+//
+// Example
+//
+//     logger.Log("Success")
+func (l Logger) Log(t string) bool {
+	if l.l < 3 {
+		return false
+	}
+	timeNow := time.Now()
+	log := timeNow.Format(time.UnixDate) + "  " + l.n + "   [LOG] - " + t
+	fmt.Println(cl.ChangeFG(log, "green"))
+	return true
+}
+
+// Grey messages just for moments when you want get some information
+//
+// Example
+//
+//     logger.Info(strconv.Itoa(len(members)))
+func (l Logger) Info(t string) bool {
+	if l.l < 4 {
+		return false
+	}
+	timeNow := time.Now()
+	log := timeNow.Format(time.UnixDate) + "  " + l.n + "  [INFO] - " + t
+	fmt.Println(cl.ChangeFG(log, "grey"))
+	return true
+}
+
+// Purple messages for debugging
+//
+// Example
+//
+//     logger.Debug("1")
+func (l Logger) Debug(t string) bool {
+	if l.l < 5 {
+		return false
+	}
+	timeNow := time.Now()
+	log := timeNow.Format(time.UnixDate) + "  " + l.n + " [DEBUG] - " + t
+	fmt.Println(cl.ChangeFG(log, "magenta"))
+	return true
+}
--- logger_test.go
+++ logger_test.go
@@ -1,25 +1,51 @@
-package logger_test
+package GoLogger_test
 
 import (
+	"strconv"
 	"testing"
 
-	lg "github.com/Jourloy/GoLogger"
+	"github.com/Jourloy/GoLogger"
 )
 
-func TestAll(t *testing.T) {
-	lg.New("all", "Test Logger")
-	lg.Log("Test log")
-	lg.Warn("Test warning")
-	lg.Info("Test info")
-	lg.Debug("Test debug")
-	lg.Error("Test error")
+func print(l GoLogger.Logger, i int, t *testing.T) {
+	if i == 1 {
+		if b := l.Error(""); b == false {
+			t.Error("Except 'Error'")
+		}
+		if b := l.Debug(""); b == true {
+			t.Error("Except false for 'Debug'")
+		}
+	} else if i == 2 {
+		if b := l.Warn(""); b == false {
+			t.Error("Except 'Warn'")
+		}
+		if b := l.Debug(""); b == true {
+			t.Error("Except false for 'Debug'")
+		}
+	} else if i == 3 {
+		if b := l.Log(""); b == false {
+			t.Error("Except 'Log'")
+		}
+		if b := l.Debug(""); b == true {
+			t.Error("Except false for 'Debug'")
+		}
+	} else if i == 4 {
+		if b := l.Info(""); b == false {
+			t.Error("Except 'Info'")
+		}
+		if b := l.Debug(""); b == true {
+			t.Error("Except false for 'Debug'")
+		}
+	} else if i == 5 {
+		if b := l.Debug(""); b == false {
+			t.Error("Except 'Debug'")
+		}
+	}
 }
 
 func TestProd(t *testing.T) {
-	lg.New("prod", "Test Logger")
-	lg.Log("Test log")
-	lg.Warn("Test warning")
-	lg.Info("Test info")
-	lg.Debug("Test debug")
-	lg.Error("Test error")
+	for i := 0; i <= 6; i++ {
+		logger := GoLogger.New(i, "I: "+strconv.Itoa(i))
+		print(logger, i, t)
+	}
 }
